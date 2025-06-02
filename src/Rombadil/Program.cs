125a6a025177fa10539db7b323cf5c3d4ae5fa54
Samuel Massé <samuelmasse4@gmail.com>
2025-06-02 00:36:28 -0700

    palette

--- src/Rombadil/Program.cs
+++ src/Rombadil/Program.cs
@@ -3,7 +3,7 @@ using Rombadil;
 var pixels = new Pixels((256, 240));
 using var canvas = new Canvas(pixels);
 
-var rom = File.ReadAllBytes(@"C:\Users\Samuel\Documents\Repos\nes-test-roms\blargg_ppu_tests_2005.09.15b\sprite_ram.nes");
+var rom = File.ReadAllBytes(@"C:\Users\Samuel\Documents\Repos\nes-test-roms\full_palette\flowing_palette.nes");
 // var rom = File.ReadAllBytes(@"C:\Users\Samuel\Desktop\NES\Super Mario Bros. (World).nes");
 
 
@@ -14,14 +14,12 @@ for (int i = 0; i <= 0x17; i++)
 var map = new ushort[0x10000];
 for (int i = 0; i < map.Length; i++)
     map[i] = (ushort)i;
-for (int i = 0; i < 0x4000; i++)
-    map[0xC000 + i] = (ushort)(0x8000 + i);
 
-var prgArea = bytes.AsSpan().Slice(0x8000, 0x4000);
-var prgRom = rom.AsSpan().Slice(0x10, 0x4000);
+var prgArea = bytes.AsSpan().Slice(0x8000, 0x8000);
+var prgRom = rom.AsSpan().Slice(0x10, 0x8000);
 prgRom.CopyTo(prgArea);
 
-var chrRom = new byte[0x2000];
+var chrRom = rom.AsMemory().Slice(0x8010, 0x2000);
 
 var state = new CpuEmulatorState();
 var memory = new CpuEmulatorMemory(bytes, map);
