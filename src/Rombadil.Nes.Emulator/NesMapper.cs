50b1809fc635e030ffa219d8504d333aafe5765d
Samuel Massé <samuelmasse4@gmail.com>
2025-06-03 19:43:31 -0700

    fix

--- src/Rombadil.Nes.Emulator/NesMapper.cs
+++ src/Rombadil.Nes.Emulator/NesMapper.cs
@@ -6,5 +6,5 @@ public class NesMapper
     public virtual byte Read(ushort addr) => 0;
     public virtual void WriteChr(ushort addr, byte value) { }
     public virtual byte ReadChr(ushort addr) => 0;
-    public virtual int MapNametableAddr(ushort addr) => addr - 0x2000;
+    public virtual int MapNametableAddr(ushort addr) => (addr - 0x2000) % 0x800;
 }
--- src/Rombadil.Nes.Emulator/NesMapperNrom.cs
+++ src/Rombadil.Nes.Emulator/NesMapperNrom.cs
@@ -2,6 +2,14 @@ namespace Rombadil.Nes.Emulator;
 
 public class NesMapperNrom(Memory<byte> prg, Memory<byte> chr) : NesMapper
 {
+    private readonly byte[] chrRam = new byte[0x2000];
+
     public override byte Read(ushort addr) => prg.Span[(addr - 0x8000) % prg.Length];
-    public override byte ReadChr(ushort addr) => chr.Span[addr];
+    public override byte ReadChr(ushort addr) => chr.Length == 0 ? chrRam[addr] : chr.Span[addr];
+    public override void WriteChr(ushort addr, byte value)
+    {
+        if (chr.Length == 0)
+            chrRam[addr] = value;
+        else chr.Span[addr] = value;
+    }
 }
